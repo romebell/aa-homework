00308b346aebf95eea5b873caa2233c0df6042b0
Rome Bell <romebell@me.com>
2019-01-11 05:11:28 -0800

    Add #initialize #enqueue #dequeue #peek

---
+++ Week1/W1D5/queue.rb
@@ -0,0 +1,18 @@
+class Queue
+    def initialize
+        line = []
+    end 
+    
+    def enqueue(el)
+
+    end
+
+    def peek
+
+    end 
+end 
+
+
+Now let's write a Queue class. We will need the following instance methods: enqueue(el), dequeue, and peek.
+
+Test your code to ensure it follows the principle of FIFO.
