c1046bff20324a3364cdfabaacca6864f5879a6b
Rome Bell <romebell@me.com>
2019-01-11 05:01:52 -0800

    Complete and test stack.rb

--- Week1/W1D5/stack.rb
+++ Week1/W1D5/stack.rb
@@ -1,20 +1,28 @@
 
 class Stack
-    def initialize
+    def initialize(stack)
         # create ivar to store stack here!
-        
+        @stack = stack
     end
 
     def push(el)
         # adds an element to the stack
+        @stack + [el]
     end
 
     def pop
         # removes one element from the stack
+        @stack.take(@stack.length - 1)
     end
 
     def peek
         # returns, but doesn't remove, the top element in the stack
+        @stack[-1]
     end
 end
 
+
+stack = Stack.new([1,2,3,4,5,6])
+p stack.push(1)
+p stack.peek 
+p stack.pop
