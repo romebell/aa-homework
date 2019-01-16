d0fad47b855320da67d1bd903e54f81fd7b5d4c2
Rome Bell <romebell@me.com>
2019-01-16 08:33:49 -0800

    Add specs to dessert_spec.rb

--- Week2/W2D3/rspec_homework_desserts/spec/dessert_spec.rb
+++ Week2/W2D3/rspec_homework_desserts/spec/dessert_spec.rb
@@ -6,30 +6,50 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
-
+  let(:chef) { double("chef", name: "Boy R Dee") }
+  let(:cake) { Dessert.new("cake", 50, chef)   }
+  
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(cake.type).to eq("cake")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do 
+      expect(cake.quantity).to eq(50)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(cake.ingredients).to be_empty
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do 
+      expect { Dessert.new("red velvet", "lots of them", chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do 
+      cake.add_ingredient("milk")
+      expect(cake.ingredients).to include("milk")
+
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do 
+      cake.eat(20)
+      expect(cake.quantity).to eq(30)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { cake.eat(51) }.to raise_error("you eat too many")
+    end
   end
 
   describe "#serve" do
