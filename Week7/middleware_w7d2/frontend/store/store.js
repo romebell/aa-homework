f1fd6e69c09d105f8b4cb30aa27a6e24299e8cd5
Rome Bell <romebell@me.com>
2019-02-19 08:35:13 -0800

    Add middleware_w7d2 homework

--- Week7/middleware_w7d2/frontend/store/store.js
+++ Week7/middleware_w7d2/frontend/store/store.js
@@ -1,13 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
 
 const configureStore = (preloadedState = {}) => {
   const store = createStore(
-    rootReducer, 
-    preloadedState,
-    applyMiddleware(logger)
-    );
+    rootReducer, preloadedState);
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
--- Week7/middleware_w7d2/frontend/todo_redux.jsx
+++ Week7/middleware_w7d2/frontend/todo_redux.jsx
@@ -5,19 +5,79 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const addLoggingToDispatch = store => next => action => {
-    let dispatch = store.dispatch(action);
-  };
-
-  const middleware = store => next => action => {
-    let result = next(action);
-    return result;
-  }
-
+  // Phase 1: addLoggingToDispatch
+  
   const preloadedState = localStorage.state ?
-    JSON.parse(localStorage.state) : {};
+  JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
-
+  store.dispatch = addLoggingToDispatch(store);
+  
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+
+
+
+const addLoggingToDispatch = store => {
+  
+  let storeDispatch = store.dispatch;
+  
+  return action => {
+    console.log(store.getState());
+    console.log(action);
+    storeDispatch(action);
+    console.log(store.getState());
+  }
+}
+
+// window.addLoggingToDispatch = addLoggingToDispatch;
+
+// const addLoggingToDispatch = store => next => action => {
+//   let dispatch = store.dispatch(action);
+
+// };
+
+// const middleware = store => next => action => {
+//   let result = next(action);
+//   return result;
+// }
+
+
+// // taken from the notes.
+// const calculator = function (operationCb) {
+//   return function (op1, op2) {
+//     console.log(`calling with ${op1} ${op2}`)
+//   }
+// }
+
+
+// const RECEIVE_MESSAGE = "RECEIVE_MESSAGE";
+// const thunkActionCreator = () => dispatch => {
+//   dispatch({
+//     type: RECEIVE_MESSAGE,
+//     message: "This will be dispatch immediately."
+//   });
+
+//   setTimeout(() => dispatch ({
+//     type: RECEIVE_MESSAGE,
+//     message: "This will be dispatch 1 second later."
+//   }, 1000));
+// }
+
+// // middleware/thunk_middleware.js
+// const thunk = ({dispatch, getState}) => next => action => {
+//   if (typeof action === 'function') {
+//     return action(dispatch, getState);
+//   }
+//   return next(action);
+// }
+
+
+
+
+
+
+
+
+
