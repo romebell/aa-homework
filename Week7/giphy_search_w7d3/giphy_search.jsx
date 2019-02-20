25b59f809778e975aef8cba7917f5180cccb5ef5
Rome Bell <romebell@me.com>
2019-02-19 22:16:44 -0800

    Unzip giphy_search and install dependencies and configure webpack

---
+++ Week7/giphy_search_w7d3/giphy_search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+
+import { createStore } from 'redux'
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  const store = createStore(reducer);
+  ReactDOM.render(<Root store={store} />, root);
+});
---
+++ Week7/giphy_search_w7d3/util/api_util.js
@@ -0,0 +1,6 @@
+const fetchSearchGiphys = (searchTerm) => {
+  return $.ajax({
+    method: 'GET',
+    url: 'http://api.giphy.com/v1/gifs/search?q=${search+term}&api_key=dc6zaTOxFJmzC&limit=2'
+  }).then(res => console.log(res));
+}
