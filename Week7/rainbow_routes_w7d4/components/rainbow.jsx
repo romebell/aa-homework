5ce050b2d3497b686821d5b488c22f0ab01fae76
Rome Bell <romebell@me.com>
2019-02-21 00:56:06 -0800

    Add all Routes for rainbow project

--- Week7/rainbow_routes_w7d4/components/rainbow.jsx
+++ Week7/rainbow_routes_w7d4/components/rainbow.jsx
@@ -19,6 +19,16 @@ class Rainbow extends React.Component {
 
         <div id="rainbow">
           {/* Your routes should go here */}
+          <Route path="/" component={Rainbow}/>
+          <Route path="/red" component={Red}/>
+          <Route path="/red/orange" component={Orange}/>
+          <Route path="/red/yellow" component={Yellow}/>
+          <Route path="/green" component={Green}/>
+          <Route path="/blue" component={Blue} />
+          <Route path="/blue/indigo" component={Indigo}/>
+          <Route path="/violet" component={Violet}/>
+
+
         </div>
       </div>
     );
